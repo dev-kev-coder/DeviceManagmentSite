6208733427aad17b53f6fa7849185359d3ed6676
Kevin Albarran <kalbarran@inland-engineering.com>
2025-03-26 22:42:47 -0700

    basic example for how to use components, state, event handlers basically a good chuck of the basics for barebones components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './components/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // Need to add Component to Imports array for html to use component
+  imports: [RouterOutlet, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
