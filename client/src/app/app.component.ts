55d22fd7ffccc1920d11d1e17a201db3d8b043c9
Kevin Albarran <kalbarran@inland-engineering.com>
2025-03-31 22:13:59 -0700

    moved example angular to another folder. starting off with basic app component. added configs for prettier and vscode

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TestComponent } from './components/test.component';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // Need to add Component to Imports array for html to use component
-  imports: [RouterOutlet, TestComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: ` <div>
+    <h2>Clipboard Info: {{ clipboardInfo() }}</h2>
+    <button (click)="changeNameOnClipboard()">Tamper with Name</button>
+    <button (click)="changeAgeOnClipboard()">Tamper with Age</button>
+  </div>`,
+
+  styles: `h1 { font-size: 3em; } `,
 })
 export class AppComponent {
-  title = 'client';
+  testName = signal('John Doe');
+  testAge = signal(12);
+  clipboardInfo = computed(() => {
+    return `Name: ${this.testName()}, Age: ${this.testAge()}`;
+  });
+
+  changeNameOnClipboard() {
+    this.testName.set('Jane Doe');
+  }
+
+  changeAgeOnClipboard() {
+    this.testAge.set(100);
+  }
 }
