66dc2737dbb1b69a7cce8f8137dda8383a30d184
kalbarran <kalbarran@inland-engineering.com>
2025-06-03 09:58:37 -0700

    added Core dll library for agent installer.

---
+++ DeviceInfoService/src/AgentInstaller.Core/Configuration/AgentOptions.cs
@@ -0,0 +1,9 @@
+// e.g. AgentOptions.cs
+namespace AgentInstaller.Core.Configuration
+{
+    /// <summary>Placeholder - prints "Hello" so we know the DLL loaded.</summary>
+    public static class AgentOptions
+    {
+        public static string SayHello() => "Hello from AgentInstaller.Core!";
+    }
+}
