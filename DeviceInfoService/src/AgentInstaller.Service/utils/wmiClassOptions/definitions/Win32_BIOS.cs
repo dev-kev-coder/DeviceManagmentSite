a753a4dee611be66a16954c1fe99a2cac1c54004
kev-dawg <recodedkevdev@gmail.com>
2025-07-09 00:07:00 -0700

    Expanded on types

--- DeviceInfoService/src/AgentInstaller.Service/utils/wmiClassOptions/definitions/Win32_BIOS.cs
+++ DeviceInfoService/src/AgentInstaller.Service/utils/wmiClassOptions/definitions/Win32_BIOS.cs
@@ -9,10 +9,23 @@ namespace AgentInstaller.Service.utils.wmiClassOptions.definitions
     class Win32_BIOS
     {
         public string[] BIOSVersion { get; set; }
+
         public string Manufacturer { get; set; }
+
+        /**
+        * Constraints: SerialNumber must be SN from Vendor.
+        * Edge cases: SerialNumber
+        *   1.  value = "System Serial Number"; BIOS might not have SN from Vendor.
+        * **/
         public string SerialNumber { get; set; }
+        public string IdentificationCode { get; set; }
+        public string Name { get; set; }
+        public string SoftwareElementID { get; set; }
+
         public string Version { get; set; }
+
         public string Status { get; set; }
+
         public UInt16[] BiosCharacteristics { get; set; }
     }
 }
---
+++ DeviceInfoService/src/AgentInstaller.Service/utils/wmiClassOptions/definitions/Win32_ComputerSystemProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentInstaller.Service.utils.wmiClassOptions.definitions
+{
+    public class Win32_ComputerSystemProduct
+    {
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        public string IdentifyingNumber { get; set; }
+        public string Name { get; set; }
+        public string SKUNumber { get; set; }
+        public string Vendor { get; set; }
+        public string Version { get; set; }
+        public string UUID { get; set; }
+    }
+}
