0fb59a7b396090ffe4f3ca100faad77dae53b21f
kev-dawg <recodedkevdev@gmail.com>
2025-07-09 00:36:48 -0700

    even newer idea using source generators...hot dog

--- DeviceInfoService/src/AgentInstaller.Service/utils/DeviceInfo.cs
+++ DeviceInfoService/src/AgentInstaller.Service/utils/DeviceInfo.cs
@@ -53,72 +53,148 @@ namespace AgentInstaller.Service.utils
                 .CreateQueryOption<Win32_Directory>("Win32_Directory")
                 .CreateAndPopulate();
 
-
+            #region T4 templates to create classes
             // Cool experiment!!! (uses T4 templates)
             // Basically use the query to read the properties for target WMI Classnames
             // We can extract out the property name and type our managed process is expected to get
             // Ideally these are queries that would not actually happen during the run time. Should only be run on build if the files weren't already made (idk how to do this)
-           /* <#@ template language="C#" hostspecific="true" debug="false" #>
-              <#@ output extension=".cs" #>
-              <#@ assembly name="Microsoft.Management.Infrastructure" #>
-              <#@ import namespace="Microsoft.Management.Infrastructure" #>
-              <#@ import namespace="System.Linq" #>
-              <#
-                  // 1️⃣ Open a CIM session against the local machine
-                  var session = CimSession.Create(null);
+            /* <#@ template language="C#" hostspecific="true" debug="false" #>
+               <#@ output extension=".cs" #>
+               <#@ assembly name="Microsoft.Management.Infrastructure" #>
+               <#@ import namespace="Microsoft.Management.Infrastructure" #>
+               <#@ import namespace="System.Linq" #>
+               <#
+                   // 1️⃣ Open a CIM session against the local machine
+                   var session = CimSession.Create(null);
 
-                          // 2️⃣ Grab the class definition (not an instance) so we can inspect its properties
-                          var cimClass = session.GetClass(namespaceName: null, className: "Win32_BIOS");
+                           // 2️⃣ Grab the class definition (not an instance) so we can inspect its properties
+                           var cimClass = session.GetClass(namespaceName: null, className: "Win32_BIOS");
 
-                          // 3️⃣ Build a schema: property name → C# type name
-                          var schema = cimClass.CimClassProperties
-                              .ToDictionary(
-                                  prop => prop.Name,
-                                  prop => MapCimTypeToCSharp(prop.CimType)
-                              );
-              #>
-              namespace Generated
-                  {
-                      public class Win32_BIOS
-                      {
-              <#
-                  // 4️⃣ Emit one property per entry in the schema dictionary
-                  foreach (var kv in schema)
-                  {
-              #>
-                      public <#= kv.Value #> <#= kv.Key #> { get; set; }
-              <#
-                  }
-              #>
-                  }
-              }
+                           // 3️⃣ Build a schema: property name → C# type name
+                           var schema = cimClass.CimClassProperties
+                               .ToDictionary(
+                                   prop => prop.Name,
+                                   prop => MapCimTypeToCSharp(prop.CimType)
+                               );
+               #>
+               namespace Generated
+                   {
+                       public class Win32_BIOS
+                       {
+               <#
+                   // 4️⃣ Emit one property per entry in the schema dictionary
+                   foreach (var kv in schema)
+                   {
+               #>
+                       public <#= kv.Value #> <#= kv.Key #> { get; set; }
+               <#
+                   }
+               #>
+                   }
+               }
 
-              <#+   // helper method in the template to map CIM types to C# types
-                  string MapCimTypeToCSharp (CimType cimType) => cimType switch
-                  {
-                      CimType.Boolean => "bool",
-                      CimType.SInt32 => "int",
-                      CimType.SInt16 => "short",
-                      CimType.SInt64 => "long",
-                      CimType.Real32 => "float",
-                      CimType.Real64 => "double",
-                      CimType.String => "string",
-                      CimType.DateTime => "DateTime",
-                      CimType.UInt32 => "uint",
-                      CimType.UInt16 => "ushort",
-                      CimType.UInt64 => "ulong",
-                      CimType.Reference => "object",     // or a nested class
-                      CimType.Char16 => "char[]",
-                      CimType.Object => "object",
-                      CimType.ReferenceArray => "object[]",
-                      CimType.StringArray => "string[]",
-                      CimType.SInt32Array => "int[]",
-                      CimType.Real64Array => "double[]",
-                      // …add more as you need…
-                      _ => "object"
-                  };
-              #>
-            */
+               <#+   // helper method in the template to map CIM types to C# types
+                   string MapCimTypeToCSharp (CimType cimType) => cimType switch
+                   {
+                       CimType.Boolean => "bool",
+                       CimType.SInt32 => "int",
+                       CimType.SInt16 => "short",
+                       CimType.SInt64 => "long",
+                       CimType.Real32 => "float",
+                       CimType.Real64 => "double",
+                       CimType.String => "string",
+                       CimType.DateTime => "DateTime",
+                       CimType.UInt32 => "uint",
+                       CimType.UInt16 => "ushort",
+                       CimType.UInt64 => "ulong",
+                       CimType.Reference => "object",     // or a nested class
+                       CimType.Char16 => "char[]",
+                       CimType.Object => "object",
+                       CimType.ReferenceArray => "object[]",
+                       CimType.StringArray => "string[]",
+                       CimType.SInt32Array => "int[]",
+                       CimType.Real64Array => "double[]",
+                       // …add more as you need…
+                       _ => "object"
+                   };
+               #>
+             */
+            #endregion T4 templates to create classes
+
+
+            #region Source generators to create classes
+            // EVEN CRAZIER IDEA!!!: Use source generators
+            /*
+                using System;
+                using System.Linq;
+                using System.Text;
+                using Microsoft.CodeAnalysis;
+                using Microsoft.CodeAnalysis.Text;
+                using Microsoft.Management.Infrastructure;
+
+                [Generator]
+                public class CimClassGenerator : ISourceGenerator
+                {
+                    public void Initialize(GeneratorInitializationContext context)
+                    {
+                        // no-op; we’re not using syntax notifications here
+                    }
+
+                    public void Execute(GeneratorExecutionContext context)
+                    {
+                        // 1️⃣ Open a CIM session (localhost)
+                        using var session = CimSession.Create(null);
+
+                        // 2️⃣ Query the class definition
+                        //    You could make this configurable via AdditionalFiles or an attribute
+                        var cimClass = session.GetClass(namespaceName: null, className: "Win32_BIOS");
+
+                        // 3️⃣ Build a map of property → C# type
+                        var props = cimClass.CimClassProperties
+                            .Select(p => (Name: p.Name, CsType: MapCimType(p.CimType)))
+                            .ToArray();
+
+                        // 4️⃣ Emit source
+                        var sb = new StringBuilder();
+                        sb.AppendLine("// <auto-generated/>");
+                        sb.AppendLine("namespace Generated;");
+                        sb.AppendLine("public class Win32_BIOS");
+                        sb.AppendLine("{");
+                        foreach (var (Name, CsType) in props)
+                        {
+                            sb.AppendLine($"    public {CsType} {Name} {{ get; set; }}");
+                        }
+                        sb.AppendLine("}");
+
+                        // 5️⃣ Add it into the compilation
+                        context.AddSource("Win32_BIOS.g.cs", SourceText.From(sb.ToString(), Encoding.UTF8));
+                    }
+
+                    private static string MapCimType(CimType cim)
+                        => cim switch
+                        {
+                            CimType.Boolean      => "bool",
+                            CimType.String       => "string",
+                            CimType.SInt32       => "int",
+                            CimType.SInt16       => "short",
+                            CimType.SInt64       => "long",
+                            CimType.Real32       => "float",
+                            CimType.Real64       => "double",
+                            CimType.DateTime     => "DateTime",
+                            CimType.UInt32       => "uint",
+                            CimType.UInt16       => "ushort",
+                            CimType.UInt64       => "ulong",
+                            CimType.StringArray  => "string[]",
+                            CimType.SInt32Array  => "int[]",
+                            CimType.Real64Array  => "double[]",
+                            // … add more as needed …
+                            _                    => "object",
+                        };
+                }
+
+             */
+            #endregion Source generators to create classes
+
         }
     }
 }
