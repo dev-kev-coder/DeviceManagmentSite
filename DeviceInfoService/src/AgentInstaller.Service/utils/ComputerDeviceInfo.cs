e5deb4f6e935a34f9b83535b8d0455c36941151e
kalbarran <kalbarran@inland-engineering.com>
2025-07-29 08:28:14 -0700

    using Win32 classes directly until it bites me in the butt.

--- DeviceInfoService/src/AgentInstaller.Service/utils/ComputerDeviceInfo.cs
+++ DeviceInfoService/src/AgentInstaller.Service/utils/ComputerDeviceInfo.cs
@@ -1,25 +1,28 @@
-﻿using AgentInstaller.Service.utils.wmiClassOptions.definitions.specs.operatingSystem;
+﻿using AgentInstaller.Service.utils.wmiClassOptions.definitions.general;
+using AgentInstaller.Service.utils.wmiClassOptions.definitions.specs.operatingSystem;
 using AgentInstaller.Service.utils.wmiClassOptions.definitions.specs.peripherals;
 using AgentInstaller.Service.utils.wmiClassOptions.definitions.specs.ProcessAndStorage;
 
 namespace AgentInstaller.Service.utils.ComputerDeviceInfoPayload
 {
-    
-
     public class ComputerDeviceInfo
     {
+        //public string CurrentUser { get; set; }
+        //public UInt64 TotalPhysicalMemory { get; set; }
 
-        public string SerialNumber { get; set; }
-        public string Manufacturer { get; set; }
-        public string Brand { get; set; }
-        public string Workgroup { get; set; }
-        public string Domain { get; set; }
-        public string ComputerName { get; set; }
-        
-        public Win32_OperatingSystem[] OS { get; set; }
+        //public string SerialNumber { get; set; }
+        //public string Manufacturer { get; set; }
+        //public string Brand { get; set; }
+        //public string Workgroup { get; set; }
+        //public string Domain { get; set; }
+        //public string ComputerName { get; set; }
+        public Win32_ComputerSystem ComputerSystemInfo { get; set; }      
+        public Win32_OperatingSystem OS { get; set; }
+        public Win32_BIOS BIOS { get; set; }
         public Win32_Processor[] Processor { get; set; }
         public Win32_PhysicalMemory[] RAM { get; set; }
         public Win32_DiskDrive[] DiskDrives { get; set; }
         public Win32_VideoController[] VideoControllers { get; set; }
+        public UsersInfoDTO UsersInfo { get; set; }
     }
 }
