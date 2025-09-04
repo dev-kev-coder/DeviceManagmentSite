377efcf255e4e309e0aca64f8ebeea148ed68659
kev-dawg <recodedkevdev@gmail.com>
2025-09-03 22:25:47 -0700

    util for hashing file contents by file path

---
+++ DeviceInfoService/src/AgentInstaller.DeviceInfo/Utils/FileHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentInstaller.DeviceInfo.Utils
+{
+    public class FileHelper
+    {
+        /**
+         * Notes:
+         *  Naive Idea - accept enum as AlgoName and create a switch statement
+         *               switch statement is in charge of create the appropriate hash algo (i.e. SHA256, MD5, etc.)
+         *               Sample api below
+         *               public static string CreateFileHash(string filePath, string algorithmName = "SHA256")
+         *               
+         * **/
+        public static string CreateFileHash(string filePath)
+        {
+            using var algo = SHA256.Create();
+            //using var algoChatGPT = HashAlgorithm.Create(algorithmName);
+
+            using var stream = File.OpenRead(filePath);
+
+            var hash = algo.ComputeHash(stream);
+
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+        }
+    }
+}
